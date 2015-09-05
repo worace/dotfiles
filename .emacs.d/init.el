00ff459a2a7f31ecbc59d1144b594ff06de56edd
worace <horace.d.williams@gmail.com>
2015-09-05 15:46:25 -0700

    one more

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,29 +5,7 @@
 (load-file "./packages.el")
 (load-file "./ui.el")
 (load-file "./theme.el")
-
-;; Enable helm autofilter/complete interface
-(require 'helm-config)
-;; Use helm-M-x as default finder in M-x
-(global-set-key (kbd "M-x") 'helm-M-x)
-;; Additionally, enable helm for file-finding
-;; And some other standard uses
-(helm-mode 1)
-
-;; Evil (vim) Mode
-(require 'evil)
-(evil-mode 1)
-
-;; Clojure Setup
-(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
-(require 'smartparens-config)
-(smartparens-global-mode t)
-(add-hook 'clojure-mode-hook #'smartparens-strict-mode)
-
-;; Markdown Setup
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(load-file "./modes.el")
 
 ;; Configure Backup Files - Put them all in home dir
 (setq backup-directory-alist `(("." . "~/.emacs_backups")))
---
+++ .emacs.d/modes.el
@@ -0,0 +1,22 @@
+;; Enable helm autofilter/complete interface
+(require 'helm-config)
+;; Use helm-M-x as default finder in M-x
+(global-set-key (kbd "M-x") 'helm-M-x)
+;; Additionally, enable helm for file-finding
+;; And some other standard uses
+(helm-mode 1)
+
+;; Evil (vim) Mode
+(require 'evil)
+(evil-mode 1)
+
+;; Clojure Setup
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+(require 'smartparens-config)
+(smartparens-global-mode t)
+(add-hook 'clojure-mode-hook #'smartparens-strict-mode)
+
+;; Markdown Setup
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
