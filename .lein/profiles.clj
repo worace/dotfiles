4ca60903d9cd35f78984a37234cc99ee7eb5773c
Horace Williams <horace.d.williams@gmail.com>
2015-11-10 06:39:36 -0700

    add template plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[lein-exec "0.3.4"]
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+	          [lein-create-template "0.1.1"]
+                  [cider/cider-nrepl "0.9.1"]
                   #_[refactor-nrepl "0.3.0-20150401.112429-28" :exclusions [org.clojure/clojure]]
                   [com.jakemccrary/lein-test-refresh "0.6.0" :exclusions [org.clojure/tools.namespace]]]
         :dependencies [[alembic "0.3.2"]
