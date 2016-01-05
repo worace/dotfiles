12d23fccf0a11f52601a6ae376452642d25c0763
Horace Williams <horace.d.williams@gmail.com>
2016-01-04 21:31:12 -0700

    try a fix for resizing line-number gutter
    never had this working quite right so whenever
    font was resized the line gutter wouldn't resize
    to match. often meant line nums got cut off
    when the font size was increased

--- .emacs.d/customizations/theme.el
+++ .emacs.d/customizations/theme.el
@@ -29,5 +29,15 @@
       indent-tabs-mode nil)
 
 (global-linum-mode)
+(defun linum-update-window-scale-fix (win)
+  "fix linum for scaled text"
+  (set-window-margins win
+		      (ceiling (* (if (boundp 'text-scale-mode-step)
+				      (expt text-scale-mode-step
+					    text-scale-mode-amount) 1)
+				  (if (car (window-margins))
+				      (car (window-margins)) 1)
+				  ))))
+(advice-add 'linum-update-window :after 'linum-update-window-scale-fix)
 
 (setq-default show-trailing-whitespace t)
