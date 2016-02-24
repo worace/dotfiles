d81e91e8ff5afa5d0b2b843f445f18ffd97eae95
Horace Williams <horace.d.williams@gmail.com>
2016-02-24 08:56:11 -0700

    weird text sizing is just too weird
    back to per-buffer default

--- .emacs.d/customizations/keybindings.el
+++ .emacs.d/customizations/keybindings.el
@@ -6,10 +6,14 @@
 (evil-leader/set-key "x" 'helm-M-x)
 (evil-leader/set-key "s" 'save-buffer)
 (evil-leader/set-key "/" 'comment-or-uncomment-region)
-(evil-leader/set-key "-" 'default-text-scale-decrease)
-(evil-leader/set-key "=" 'default-text-scale-increase)
+(evil-leader/set-key "-" 'text-scale-decrease)
+(evil-leader/set-key "=" 'text-scale-increase)
 (evil-leader/set-key "w" 'ace-window)
 (evil-leader/set-key "ct" 'toggle-theme)
+
+(setq mac-command-modifier 'super)
+(global-set-key (kbd "s--") 'text-scale-decrease)
+(global-set-key (kbd "s-=") 'text-scale-increase)
 
 ;;Clojure
 (evil-leader/set-key "eb" 'cider-eval-buffer)
@@ -58,6 +62,22 @@
   "Move the current line down by N lines."
   (interactive "p")
   (move-line (if (null n) 1 n)))
+
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME.
+   Borrowed from Steve Yegge's .emacs: https://sites.google.com/site/steveyegge2/my-dot-emacs-file"
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
 
 ;; (evil-define-key 'normal global-map (kbd "S-k") 'move-line-up)
 ;; (define-key evil-normal-state-map (kbd "S-J") 'move-line-down)
