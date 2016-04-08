2a58a011ce9653517a6d04c19f8a21e685441f7a
Horace Williams <horace.d.williams@gmail.com>
2016-04-07 19:23:32 -0600

    add hotload deps fn for lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
 ;;Hotloading deps with refactor-nrepl:
 ;;https://gist.github.com/worace/742d73e8d27a46ad759c
+
 {:user {:plugins [[lein-exec "0.3.4"]
                   [lein-try "0.4.3"]
 	          [lein-create-template "0.1.1"]
@@ -7,6 +8,12 @@
                   [refactor-nrepl "1.1.0"]
                   [com.jakemccrary/lein-test-refresh "0.6.0" :exclusions [org.clojure/tools.namespace]]]
         :injections [(require ['clojure.repl :refer ['doc]])]
+        :repl-options {
+                       :init (do (println "HI")
+                                 (defn hotload-dependency [dep-name version-string]
+                                   (refactor-nrepl.artifacts/hotload-dependency
+                                    {:coordinates (str "[" dep-name " " "\"" version-string "\"]")})))
+                       }
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.namespace "0.2.11"]
                        [org.clojure/tools.nrepl "0.2.12"]]
