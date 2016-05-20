acbbc23b54c8a73fe64f8738679c9a21eced09a5
Horace Williams <horace.d.williams@gmail.com>
2016-05-20 13:49:05 -0600

    little extra setup for webmode

--- .emacs.d/customizations/modes.el
+++ .emacs.d/customizations/modes.el
@@ -98,7 +98,10 @@
   "Hooks for Web mode."
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
   (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-auto-closing t)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2))
 (add-hook 'web-mode-hook  'my-web-mode-hook)
