573e405d8559326afcbbc5d80d589a72af401685
Horace Williams <horace.d.williams@gmail.com>
2016-06-08 09:01:36 -0600

    Move emacs config to non-hidden directory to make searching work more naturally

--- emacs/customizations/modes.el
+++ emacs/customizations/modes.el
@@ -139,12 +139,21 @@
 (setq org-src-preserve-indentation nil
       org-edit-src-content-indentation 0)
 
+(defun org-insert-example-block ()
+  (interactive)
+  (progn
+    (newline-and-indent)
+    (insert "#+BEGIN_EXAMPLE\n")
+    (newline-and-indent)
+    (insert "#+END_SRC\n")
+    (previous-line 2)
+    (org-edit-src-code)))
 
 (defun org-insert-src-block (src-code-type)
   "Insert a `SRC-CODE-TYPE' type source code block in org-mode."
   (interactive
    (let ((src-code-types
-          '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++" "css"
+          '("emacs-lisp" "python" "C" "sh" "shell" "java" "js" "clojure" "C++" "css"
             "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
             "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk" "ditaa"
             "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
