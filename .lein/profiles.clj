07e843b3bd54c9f900cade292e9daf4ddd3bd3b9
Horace Williams <horace.d.williams@gmail.com>
2016-08-05 20:48:24 -0600

    humane test output is pretty neat

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -7,13 +7,16 @@
                   [org.clojure/tools.namespace "0.2.11"]
                   [refactor-nrepl "1.1.0"]
                   [com.jakemccrary/lein-test-refresh "0.14.0" :exclusions [org.clojure/tools.namespace]]]
-        :injections [(require ['clojure.repl :refer ['doc]])]
+        :injections [(require ['clojure.repl :refer ['doc]])
+                     (require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
         :repl-options {
                        :init (do (defn hotload-dependency [dep-name version-string]
                                    (refactor-nrepl.artifacts/hotload-dependency
                                     {:coordinates (str "[" dep-name " " "\"" version-string "\"]")})))
                        }
         :dependencies [[alembic "0.3.2"]
+                       [pjstadig/humane-test-output "0.8.0"]
                        [org.clojure/tools.namespace "0.2.11"]
                        [org.clojure/tools.nrepl "0.2.12"]]
         :jvm-opts ["-Djava.net.preferIPv4Stack=true" "-XX:-OmitStackTraceInFastThrow"]}}
