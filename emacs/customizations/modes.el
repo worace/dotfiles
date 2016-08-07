b8008423cfe0a2a6c7541e3d54b5863f6cdfe882
Horace Williams <horace.d.williams@gmail.com>
2016-08-07 11:27:18 -0600

    fix how i was setting filetypes for web mode

--- emacs/customizations/modes.el
+++ emacs/customizations/modes.el
@@ -116,17 +116,22 @@
             (local-set-key (kbd "C-c C-k")
                            'python-shell-clear-output)))
 
+
 (require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-enable-auto-pairing t)
   (setq web-mode-enable-auto-closing t)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2))
+
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
 
