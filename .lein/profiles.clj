47436354603c3c16a7666dedb76ccec737b6c0ef
Horace Williams <horace.d.williams@gmail.com>
2016-09-14 09:03:14 -0400

    give up on nrepl stuff for now in lein config

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -6,14 +6,10 @@
 	          [lein-create-template "0.1.1"]
                   [org.clojure/tools.namespace "0.2.11"]
                   [com.jakemccrary/lein-test-refresh "0.14.0" :exclusions [org.clojure/tools.namespace]]]
-        :injections [(require ['clojure.repl :refer ['doc]])
+        :injections [(require '[clojure.repl :refer [doc]])
                      (require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
-        :repl-options {
-                       :init (do (defn hotload-dependency [dep-name version-string]
-                                   (refactor-nrepl.artifacts/hotload-dependency
-                                    {:coordinates (str "[" dep-name " " "\"" version-string "\"]")})))
-                       }
+        :repl-options {:init (do )}
         :dependencies [[alembic "0.3.2"]
                        [pjstadig/humane-test-output "0.8.0"]
                        [org.clojure/tools.namespace "0.2.11"]
