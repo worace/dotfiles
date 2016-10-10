78a3a5af8be2e91171ec9a668e78763480031868
Horace Williams <horace.d.williams@gmail.com>
2016-10-10 15:43:19 -0400

    Sorting out more JS mode and eslint flycheck options

--- emacs/customizations/modes.el
+++ emacs/customizations/modes.el
@@ -72,12 +72,40 @@
                 "clearTimeout" "setInterval" "clearInterval" "location"
                 "__dirname" "console" "JSON" "describe" "it" "beforeEach"
                 "before" "after" "afterEach"))
+
+(setq-default js2-bounce-indent-p t
+              js2-include-node-externs t
+              js2-include-browser-externs t
+              js2-basic-offset 2
+              js2-mode-show-parse-errors t
+              js2-mode-show-strict-warnings t
+              js2-strict-trailing-comma-warning nil
+              js2-strict-missing-semi-warning t
+              js2-strict-inconsistent-return-warning nil)
+
 (setq js2-bounce-indent-p t)
 (setq js2-basic-offset 2)
 (setq js2-include-node-externs t)
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (autoload 'jsx-mode "jsx-mode" "JSX mode" t)
+
+(setq flycheck-disabled-checkers '(javascript-jshint))
+(setq flycheck-checkers '(javascript-eslint))
+
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+(add-hook 'js2-mode-hook #'flycheck-mode)
+
 
 (require 'nvm)
 (defun do-nvm-use (version)
@@ -124,7 +152,11 @@
                         (read-only-mode)
                         (goto-char (point-max))
                         (local-set-key (kbd "q")
-                                       (lambda () (interactive) (quit-restore-window)))))))
+                                       (lambda () (interactive) (quit-restore-window))))
+                      nil
+                      ;; This can be any truthy value but prevents this compilation hook
+                      ;; from bleeding over to other buffers
+                      'make-it-buffer-local)))
 
 (require 'ruby-mode)
 
