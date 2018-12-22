09aeb30409307b447c087f0e3f1a5d4b85060dae
Horace Williams <horace.d.williams@gmail.com>
2018-12-21 21:15:46 -0800

    TSX / typescript config

--- emacs/customizations/modes.el
+++ emacs/customizations/modes.el
@@ -583,8 +583,17 @@
 
 ;; formats the buffer before saving
 (add-hook 'before-save-hook 'tide-format-before-save)
-
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; TSX with Web Mode + Tide
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
 
 (add-hook 'mmm-mode-hook
           (lambda ()
