bca4f55324e68151e3604a241820e7e92f461f85
Horace Williams <horace.d.williams@gmail.com>
2021-05-22 02:41:13 -0700

    update emacs rust config

--- emacs/customizations/modes.el
+++ emacs/customizations/modes.el
@@ -315,26 +315,30 @@
     (previous-line 2)
     (org-edit-src-code)))
 
-(use-package flycheck-rust)
-;; TODO - Something about this breaks Scala / LSP setup
-;; (use-package rust-mode
-  ;; :hook ((rust-mode . #'cargo-minor-mode)
-  ;;        (rust-mode . #'racer-mode)
-  ;;        (rust-mode . #'flycheck-mode)
-  ;;        ;; (flycheck-mode . #'flycheck-rust-setup)
-  ;;        )
-  ;; :config
-  ;; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-  ;; (evil-leader/set-key-for-mode 'rust-mode "TAB" 'rust-format-buffer)
-  ;; )
+;; https://robert.kra.hn/posts/2021-02-07_rust-with-emacs/
+(use-package rustic
+  ;; :bind (:map rustic-mode-map
+  ;;             ("M-j" . lsp-ui-imenu)
+  ;;             ("M-?" . lsp-find-references)
+  ;;             ("C-c C-c l" . flycheck-list-errors)
+  ;;             ("C-c C-c a" . lsp-execute-code-action)
+  ;;             ("C-c C-c r" . lsp-rename)
+  ;;             ("C-c C-c q" . lsp-workspace-restart)
+  ;;             ("C-c C-c Q" . lsp-workspace-shutdown)
+  ;;             ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
 
-;; ;; (use-package racer
-;; ;;   :defer t
-;; ;;   :after rust-mode
-;; ;;   :hook
-;; ;;   (racer-mode . #'eldoc-mode)
-;; ;;   (racer-mode . #'company-mode))
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
 
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm
+  (setq-local buffer-save-without-query t))
 
 (use-package alchemist
   :hook ((alchemist-iex-mode . show-trailing-whitespace)
